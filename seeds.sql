bc42fd2e53262d7223c34f52e5ed051d933fc4ab
Chris Reed <cr31293@gmail.com>
2020-09-22 20:30:44 -0400

    updated seed file to correctly format

--- seeds.sql
+++ seeds.sql
@@ -1,19 +1,28 @@
 USE tracker;
 
-INSERT INTO department (id)
-VALUES ("1001"), ("1002"), ("1003");
+INSERT INTO department (id, full_name)
+VALUES ("1000", "Chris Reed");
 
-INSERT INTO department (name)
-VALUES ("Chris Reed"), ("Meghan Iman"), ("Lisa Reed");
+INSERT INTO department (id, full_name)
+VALUES ("1001", "Meghan Iman");
 
-INSERT INTO role (id)
-VALUES ("1001"), ("1002"), ("1003");
+INSERT INTO department (id, full_name)
+VALUES ("0001", "Lisa Reed");
 
-INSERT INTO role (title)
-VALUES ("Engineer"), ("Manager"), ("Founder");
+INSERT INTO roles (id, title, salary, department_id)
+VALUES ("1000", "Engineer", 65000.00, 124);
 
-INSERT INTO role (salary)
-VALUES ("60,000"), ("80,000"), ("");
+INSERT INTO roles (id, title, salary, department_id)
+VALUES ("1001", "Manager", 80000.00, 124);
 
-INSERT INTO role (department_id)
-VALUES ("1020"), ("1020"), ("0000");
+INSERT INTO roles (id, title, salary, department_id)
+VALUES ("0001", "Founder", 1.00, 001);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES ("1000", "Chris", "Reed", "564", "444");
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES ("1001", "Meghan", "Iman", "500", NULL);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES ("0001", "Lisa", "Reed", "564", NULL);
